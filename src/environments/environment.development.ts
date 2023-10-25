112db96e36cd897da0b1c1ea6a33298dddf94d8d
Gustavo <gustascrows@gmail.com>
2023-10-25 19:12:06 -0300

    feat: correcao auth0

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     apiUrl: '',
-    auth0Domain: '',
-    auth0ClientId: ''
+    auth0Domain: 'dev-3yg4atan6fctzshg.us.auth0.com',
+    auth0ClientId: '5FQMuVFkzawsOGDWnNRT1HV0qvHrCbQf',
 };
