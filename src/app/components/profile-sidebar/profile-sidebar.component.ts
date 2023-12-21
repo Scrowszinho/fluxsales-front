71515b3e94643b0031bb13bdccaab66f8fa85e63
Gustavo <gustascrows@gmail.com>
2023-12-20 21:51:52 -0300

    feat: atualizando perfil

--- src/app/components/profile-sidebar/profile-sidebar.component.ts
+++ src/app/components/profile-sidebar/profile-sidebar.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { PipesModule } from 'src/app/pipes/pipes.module';
 
 @Component({
   selector: 'app-profile-sidebar',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, CommonModule, PipesModule],
   templateUrl: './profile-sidebar.component.html',
   styleUrl: './profile-sidebar.component.scss'
 })
