a67943c34c745845a1c9e15d63f42a1508c00742
Gustavo <gustascrows@gmail.com>
2023-12-22 18:02:29 -0300

    feat: importando new product

--- src/app/modules/product/product.routing.ts
+++ src/app/modules/product/product.routing.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductComponent } from './product.component';
+import { NewProductComponent } from './new-product/new-product.component';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
       { path: '', component: ProductComponent },
+      { path: 'new-product', component: NewProductComponent }
     ]),
   ],
   exports: [RouterModule],
